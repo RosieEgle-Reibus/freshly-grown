20760ea02b41f59c33de862634ded45f007c0d5d
Rose Egle <rosiemegle89@gmail.com>
2019-11-22 15:21:20 -0500

    created app, project and models, migrated models

---
+++ freshly_grown_project/freshly_grown_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FreshlyGrownAppConfig(AppConfig):
+    name = 'freshly_grown_app'
---
+++ freshly_grown_project/freshly_grown_app/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Farm(models.Model):
+    name = models.CharField(max_length=100, default='n/a')
+    description = models.CharField(max_length=255, default='n/a')
+    location = models.CharField(max_length=100, default='n/a')
+    farm_pic_url = models.CharField(max_length=255, default='n/a')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, default='n/a')
+    description = models.CharField(max_length=255, default='n/a')
+    price = models.IntegerField(default='n/a')
+    unit = models.CharField(max_length=20, default='lbs')
+    total_quantity = models.IntegerField(default='0')
+    order_quantity = models.IntegerField(default='0')
+    product_pic_url = models.CharField(max_length=255, default='n/a')
+    tag = models.CharField(max_length=30, default='n/a')
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE, related_name= 'products')
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, default='n/a')
+    description = models.CharField(max_length=255, default='n/a')
+    location = models.CharField(max_length=100, default='n/a')
+    rest_pic_url = models.CharField(max_length=255, default='n/a')
+
+    def __str__(self):
+        return self.name
+
