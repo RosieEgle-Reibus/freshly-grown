cde8928fe03ddf2dde1611bd4f28b60c8cdfefa2
Rose Egle <rosiemegle89@gmail.com>
2019-11-23 12:04:03 -0500

    Working on app.js to map through both farms and their corresponding products

--- freshly_grown_project/client/src/App.js
+++ freshly_grown_project/client/src/App.js
@@ -1,18 +1,63 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import './App.css'
 import AllFarms from './component/AllFarms'
-
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  state = {
+    allFarms: [],
+    productList: []
+  }
+  componentDidMount(){
+      axios.get('/api/v1/farm')
+      .then((res) => {
+          console.log(res.data)
+          const allFarms = res.data
+          this.setState({allFarms: allFarms}) 
+          
+      })
+  }
+
+  getProducts = () => {
+    {this.state.allFarms.map((farm) => {
+      let productList = []
+      productList = farm.products
+      this.setState({productList: productList })
+      return(
+        <div>
+          {this.state.productList}
+          </div>
+      )
+    })}
+  }
+
+  productMapTry = (products) => {
+    {products.map((product) => {
+      console.log(product.name, product.description)
+      return (product.name, product.description)
+      
+    })}
+  }
+
   render() {
     return (
       <Router>
       <div>
-        
+        {this.state.allFarms.map((farm) =>{
+          return(
+            <div>
+            <Link to={`/farm/${farm.id}`}>
+              <h1>{farm.name}</h1> 
+            </Link>
+            <h2>{this.productMapTry(farm.products)}</h2>
+            </div>
+          )
+        })}
+  
       </div>
       <Switch>
-        <Route exact path='/' component={AllFarms}/>
+        {/* <Route exact path='/' component={AllFarms}/> */}
 
       </Switch>
       </Router>
--- freshly_grown_project/client/src/component/AllFarms.jsx
+++ freshly_grown_project/client/src/component/AllFarms.jsx
@@ -5,16 +5,7 @@ import {Link} from 'react-router-dom'
 
 
 export default class AllFarms extends Component {
-    state = {
-        AllFarms: []
-    }
 
-    componentDidMount(){
-        axios.get('/api/v1/farm')
-        .then((res) => {
-            console.log(res.data)
-        })
-    }
 
     render() {
         return (
