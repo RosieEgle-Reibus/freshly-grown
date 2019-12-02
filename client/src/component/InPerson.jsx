cc363b15880c760b866b6d35d82022dda4681925
Rose Egle <rosiemegle89@gmail.com>
2019-12-02 17:25:37 -0500

    some styles for market page and split function for output

--- client/src/component/InPerson.jsx
+++ client/src/component/InPerson.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../Market.css'
 import axios from 'axios'
 
 
@@ -8,51 +9,64 @@ export default class InPerson extends Component {
         zip: ''
 
     }
-  
+
     onChangeZipForm = (event) => {
-        const previousState =  {...this.state.zip}
+        const previousState = { ...this.state.zip }
         previousState[event.target.name] = event.target.value
-        this.setState({zip: previousState.zip})
+        this.setState({ zip: previousState.zip })
     }
 
     searchZip = (event) => {
         event.preventDefault()
         axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${this.state.zip}`)
-        .then((res) => {
-            console.log(res.data)
-            this.setState(res.data)
-        })
-        
+            .then((res) => {
+                console.log(res.data)
+                this.setState(res.data)
+            })
+
     }
 
+    splitStringMiles = (string) => {
+        return string.substr(0, string.indexOf(' '))
+    }
+
+    splitStringName = (string) => {
+        return string.substr(string.indexOf(' ') + 1)
+    }
 
     render() {
         return (
-            <div>
-               <form>
-                   <input 
-                   type="string"
-                   placeholder="Zip Code"
-                   id="zip"
-                   value={this.state.zip}
-                   name="zip"
-                   onChange={this.onChangeZipForm}
-                   />
-                   <input 
-                   type="submit"
-                   value="Search for Markets"
-                   onClick={this.searchZip}/>
+            <div className="market-container">
+                
+                <h1 className="all-title">Find a Market Near You</h1>
+                <form>
+                    <label for="zip" className="zip-label">Enter Your Zip Code</label>
+                    <input
+                        type="string"
+                        
+                        id="zip"
+                        value={this.state.zip}
+                        name="zip"
+                        onChange={this.onChangeZipForm}
+                    />
+                    <input
+                        type="submit"
+                        value="Search "
+                        onClick={this.searchZip} />
 
-               </form>
+                </form>
 
                 {this.state.results.map((market) => {
-                    return(
+                    return (
                         <div>
-                            <h1>{market.marketname}</h1>
-                            </div>
+                            <h1>{this.splitStringMiles(market.marketname)} Miles Away</h1>
+                            <h1>{this.splitStringName(market.marketname)}</h1>
+
+                        </div>
                     )
                 })}
             </div>
+           
         )
     }
 }
